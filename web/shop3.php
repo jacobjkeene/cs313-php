91c7d9d85bec9453066bc1e428d7b4244edc3186
Jacob Keene <Jacob Keene>
2017-05-06 16:06:40 -0600

    edited shop cart

--- web/shop3.php
+++ web/shop3.php
@@ -17,6 +17,7 @@
 		?>
 		</ul>
 		<br/>
+		<p>If your Shopping Cart doesn't look right, please go back to the browse page and try again.</p>
 		<a href="browse3.php">Back to Browse Items</a>
 		<a href="checkout3.php">Continue to Checkout</a>
 	</body>
