0edc6181375e306283ef0459fc5b914d0080e9bf
jacobjkeene <jacobjkeene@gmail.com>
2017-05-20 16:36:15 -0600

    links

--- web/prove5.php
+++ web/prove5.php
@@ -30,15 +30,15 @@ catch (PDOException $ex) {
 }
 
 ?>
-
+<h1>Welcome!</h1>
 <p>Click the following link to see our Users in the database:</p>
-<a href="../users5.php>">Users</a>
+<a href="C:\Users\jacob\cs313-php\web\users5.php>">Users</a>
 
 <p>Click the following link to see our Shows in the database:</p>
-<a href="../shows5.php>">Shows</a>
+<a href="..\shows5.php>">Shows</a>
 
 <p>Click the following link to see our Ratings in the database:</p>
-<a href="../ratings5.php>">Ratings</a>
+<a href="...\ratings5.php>">Ratings</a>
 
 </body>
 </html>
